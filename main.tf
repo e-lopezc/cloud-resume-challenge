e50c62757432336a7dda7e7ae81bfaa92fbaffc2
Elias Lopez <eliasjavier@gmail.com>
2025-01-08 20:34:44 +0000

    fixing definition for cloudfront module

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "s3_website_bucket" {
 }
 
 module "cloud_distribution" {
-  source                     = "./modules/cloudfront"
+  source                                 = "./modules/cloudfront"
   s3_bucket_website_regional_domain_name = module.s3_website_bucket.website_bucket_regional_domain_name
-  bucket_name                = module.s3_website_bucket.website_bucket_domain_name
-  depends_on                 = [module.s3_website_bucket] 
+  bucket_name                            = module.s3_website_bucket.website_bucket_domain_name
+  depends_on                             = [module.s3_website_bucket]
 }
