aed1162f3a7249564962794abbf23bb15034b896
Elias Lopez <eliasjavier@gmail.com>
2025-03-05 21:10:49 +0000

    removing redundant js file

--- mycvfiles/visits_counter.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("YOUR LAMBDA FUNCTION URL HERE");
-    let data = await response.json();
-    counter.innerHTML = `${data}`;
-
-}
-
-updateCounter();
